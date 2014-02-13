a3d7b7560911fff18f4d5975014c8a38fd8ae6c2
James Meldrum <james@htmldrum.com>
2014-02-13 08:28:36 -0500

    Added post on corporate memory of computing.

--- 2014-02-13.sh
+++ 2014-02-13.sh
@@ -19,13 +19,67 @@
 set -o nounset                              # Treat unset variables as an error
 
 
-Technology and the
-==================
+Digital Selection
+=================
 
-$BODY
+-- Statement of identity with the problem
+I'm not particularly interested in start ups or the latest jQuery plugin. Never
+have been. I see the success of computing in the present age as a fluke. Owing
+its success to new academic theories of computation, that we still have not had time to
+properly digest into the rest of society or that we have the context to properly
+appreciate ourselves. It's a beautiful thing. A beauty that each user feels when
+they discover a application that 'fits' for the first time. 
+
+-- Origins of me
+  My first memories of computation are those of the 2nd Wave. In the first wave,
+the masters set boundaries, lose motivation and cloud their original intention
+with divergent implementations. The 2nd Wave seeks to unify the first wave by
+refining the ideas that survive and updating the context.
+  Memory is unimportant but illustrating. The earliest memories I have of my
+  exposure to consumerized computing devices was indirect. Prior to moving to
+  regional Australia, I think my mother performed administration in Lend Lease
+  for the Sydney 2000 Olympic project. Wandering around the offices, with my
+    brother at the age of 5, I noticed that everyone had a computer that kept
+    them at their desk. An overlord. An observation deck. A Damoclese sword to
+    remind you that if you're not emailing your corporate, sexual oppressor with
+    your whereabouts, you're forced into abject poverty in a clock cycle. 
+I'm upset with the popularity of the glorification of computing in the 90's.
+
+Perhaps some layover from the abject intellectual poverty of the 80's, most
+media is concerned with nostalgia over legacy gaming systems which is itself
+solely a story of corporate warfare. MASTER SYSTEM v SNES v MEGA DRIVE v
+PlayStation v 64  with some
+sprinkling of Shigeru Miyamoto thrown in for humanity. Raised in a single-parent household 
+in one of the poorest communities in Australia, this consumerism is not mine.
+
+Whilst I'd prefer to keep my rememberances to myself, my first real interaction
+with a computer was at Kororo Public School in 1998. I'd just got a Hotmail
+account and fuck I loved computers. When I went to school, I had free access to
+a machine that would let me communicate with people in different worlds, didn't
+require any money to keep operating. We could create and inspire and drive (ETC>
+BAD )
+
+It was here that I started building myself.
+First web page. Pokemon Red v Blue. It was mine. And I don't have it anymore
+because of oppressive corporate computing who didn't see it as important.
+
+-- Origin of computing
+Small teams of scientific alchemists, leveraging the resources of governments hungry
+for new weapons, corporations eager for new revenue and owners of capital
+  seeking more relevance in the people they exploit. These teams came together,
+  whether out of necessity, greed or ennuit, and leveraged new tools we had for 
+
+  The FreeBSD3.2 is as much art as Matisse is.
+
+  THIS IS AMAZING OKAY?! DEAL THE FUCK WITH IT. DON'T TELL ME MARIA ABRAMOVIC
+  CONTRIBUTED MORE TO SOCIETY THAN DENNIS RITCHIE.  
+
+-- tools
 
 Plaintext.
 
 Until Next Time,
 
 James Meldrum
+
+--
